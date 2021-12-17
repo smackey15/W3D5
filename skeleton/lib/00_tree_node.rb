d760f8911c75a77088b676a711490b7da4160c3f
Deanding <dingtian.ding@gmail.com>
2021-12-17 11:53:44 -0500

    set up initialize

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
+    attr_reader :value, :parent, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
+    def parent
+
+    end
+
+    def add_child
+
+    end
 end
